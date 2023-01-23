fa87c94808eb7fd0f1caa0941c982105650bce8f
Samuel Mintah <samiblaq8@gmail.com>
2023-01-22 16:36:19 -0800

    task-manager-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import AccountData from "./components/AccountSection/AccountData";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu/Menu";
+import TasksSection from "./components/TasksSection/TasksSection";
+import ModalCreateTask from "./components/Utilities/ModalTask";
+import { Task } from "./interfaces";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { modalActions } from "./store/Modal.store";
+import { tasksActions } from "./store/Tasks.store";
+
+const App = () => {
+  const modal = useAppSelector((state) => state.modal);
+
+  const dispatch = useAppDispatch();
+
+  const closeModalCreateTask = () => {
+    dispatch(modalActions.closeModalCreateTask());
+  };
+
+  const createNewTaskHandler = (task: Task) => {
+    dispatch(tasksActions.addNewTask(task));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-200 min-h-screen text-slate-600 dark:bg-slate-900 dark:text-slate-400 xl:text-base sm:text-sm text-xs">
+      {modal.modalCreateTaskOpen && (
+        <ModalCreateTask
+          onClose={closeModalCreateTask}
+          nameForm="Add a task"
+          onConfirm={createNewTaskHandler}
+        />
+      )}
+      <Menu />
+      <TasksSection />
+      <Footer />
+      <AccountData />
     </div>
   );
-}
+};
 
 export default App;
